6a098fccb9f3a480bf71ecc32cb41968af2c8b73
suxxus <suxxus@outlook.com>
2017-06-29 11:45:42 +0200

    create IO example

--- src/scripts/05-io.js
+++ src/scripts/05-io.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function() {
 
 
@@ -6,15 +8,14 @@
 // It is important to note that upon construction of an IO instance,
 // the action will not be executed until the runIO method is called.
 
-const fs = require('fs');
-const IO = require('ramda-fantasy').IO;
-const identity = require('ramda-fantasy').Identity;
+    const fs = require('fs');
+    const IO = require('ramda-fantasy').IO;
 
 // :: String -> IO String
-const readFile = (filename) =>
+    const readFile = (filename) =>
     IO(() => fs.readFileSync(filename, 'utf8'));
 
-console.log(
+    console.log(
     readFile('test.json')
     .map(JSON.parse)
     .runIO()
@@ -26,11 +27,13 @@ console.log(
 // Produces an IO instance that when executed, will apply the given
 // function to the result of the action in this IO instance and then
 // execute the resulting IO action.
-console.log(
+    console.log(
 
     IO(() => process.argv)
      .chain(
-         (data) => IO(() => process.stdout.write(data[0]))
+         // (data) => IO(() => process.stdout.write(data[0]))
+         data => IO(()=>data[0])
+
      )
     .runIO()
 ); // => /home/diego/.nvm/versions/node/v6.9.1/bin/nodetrue
