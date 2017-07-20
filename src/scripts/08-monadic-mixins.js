492b1d20e8983c562f197c45c54437d1500b1ae0
suxxus <suxxus@outlook.com>
2017-07-20 15:22:03 +0200

    updated

--- src/scripts/08-monadic-mixins.js
+++ src/scripts/08-monadic-mixins.js
@@ -4,7 +4,7 @@ const Maybe = require('ramda-fantasy').Maybe;
 const Identity = require('ramda-fantasy').Identity;
 const E = require('ramda-fantasy').Either;
 const IO = require('ramda-fantasy').IO;
-const { isNil, compose, map, inc, toString } = require('ramda');
+const { identity, isNil, curry, compose, map, inc, add, toUpper, toString } = require('ramda');
 
 const Just = Maybe.Just;
 const Right = E.Right;
@@ -17,174 +17,77 @@ const chain = function(mma) {
     return mma.chain(x => x);
 };
 
-(function() {
-
-    const users = {
-        addresses: [{
-            street: {
-                name: 'Mulburry',
-                number: 8402,
-            },
-            postcode: 'WC2N',
-        }]
-    };
-
-    // safeProp :: Key -> {Key: a} -> Maybe a
-    const safeProp = (key) =>
-        (obj) => Maybe(obj[key]);
-
-    // safeHead :: [a] -> Maybe a
-    const safeHead = safeProp(0);
-
-    //  join :: Monad m => m (m a) -> m a
-    const chain = function(mma) {
-        return mma.chain(x => x);
-    };
-
-    //     Maybe(users.addresses)
-    //     .map(x => Maybe(x[0]))
-    //     .chain(x => x)
-    //     .map(x => Maybe(x.street))
-    //     .chain(x => x)
-    //     .toString()
-    // => Maybe.Just({"name": "Mulburry", "number": 8402})
-
-    //  firstAddressStreet :: User -> Maybe Street
-    const firstAddressStreet = compose(
-        chain,
-        map(safeProp('street')),
-        chain,
-        map(safeHead),
-        safeProp('addresses')
-    );
-
-    firstAddressStreet(users);
-    // => Maybe.Just({"name": "Mulburry", "number": 8402})
-}());
+const print = console.log.bind(console);
 
 (function() {
+    const incrementListVals = map(inc);
 
-    const log = console.log.bind(console);
-    let num;
-    //    enum Err {'no value'}
+    // Functors apply a function to a wrapped value:
+    // a => a + 3 <*> Just(1)
+    print(
+        Maybe('a')
+        .map(toUpper)
+        .toString()
+    ); // Maybe.Just("A")
 
-    const checkVals = x => isNil(x) ? Left(new Error('can not do it')) : Right(x);
-    //    const handleLef
-
-    // compose(
-    //     log,
-    //     chain,
-    //     map(x => `your values is: ${x}`),
-    //     map(inc),
-    //     map(x => Either.either(x => x, x => x)),
-    //     Either.either(()) checkVals
-    // )(num)
-
+    print(
+        Maybe([1, 3, 5])
+        .map(incrementListVals)
+        .toString()
+    ); // Maybe.Just([2, 4, 6])
 
 
-    compose(
-        //        log,
-        // chain,
-        map(x => `your values is: ${x}`),
-        map(inc),
-        // map(x => Either.either(x => x, x => x)),
-        checkVals
-    )(num);
+    // Applicatives apply a wrapped function to a wrapped value
+    // Just(a => () => a + 3) <*> Just(2)
+    print(
+        Maybe(1)
+        .map(a => fn => fn(a))
+        .ap(Just(inc))
+        .toString()
+    ); // => Maybe.Just(2)
+
+    print(
+        Maybe(3)
+        .map(add)
+        .ap(Just(2))
+        .toString()
+    ); // => Maybe.Just(5)
+
+    // Monads apply a function that returns a wrapped value to a wrapped value.
+    print(
+        Maybe(Just(4))
+        .chain(identity)
+        .map(add(4))
+        .toString()
+    ); // Maybe.Just(8)
 
 
-    const handleL = log;
+    const foo = x => Just(x);
 
-
-
-    const aux = compose(
-        chain,
-        map(x => `your values is: ${x}`),
-        Maybe,
-        either(e => log('-->', e.message), inc),
-        checkVals
-    )(num);
-
-    const aux2 = Maybe(null)
-        // .chain(x => !x ? 'none' : x)
-        .map(inc)
-        .getOrElse('noen');
-
-
-    console.log(aux2);
-
-
-
-
-    // console.log(
-    //         checkVals(num)
-    //           .map(inc)
-    //           .map(toString)
-    //           .chain(x => 'your value is ' + x)
-
-    //   );
-
-
-
-    // console.log(
-    //     Maybe(checkVals(num))
-    //     .chain(x => x)
-    //     .map(inc)
-    //     .chain(x => `your value is: ${x}`)
-
-    //   );
-
-    // console.log(
-    // Maybe(num)
-    // .chain(x => x)
-    // .toString()
-
-    // compose(
-    //         map(x => {
-    //                 console.log(x);
-    //             }
-    //             //isNothing(x) ? Left('can do it') : Right(chain(x))
-    //         ),
-    //         chain,
-    //         Maybe
-    //     )(num)
-    // );
-
-    /*
-  //  log :: a -> IO a
-var log = function(x) {
-  return new IO(function() {
-    console.log(x);
-    return x;
-  });
-};
-
-//  setStyle :: Selector -> CSSProps -> IO DOM
-var setStyle = curry(function(sel, props) {
-  return new IO(function() {
-    return jQuery(sel).css(props);
-  });
-});
-
-//  getItem :: String -> IO String
-var getItem = function(key) {
-  return new IO(function() {
-    return localStorage.getItem(key);
-  });
-};
-
-//  applyPreferences :: String -> IO DOM
-var applyPreferences = compose(
-  join, map(setStyle('#main')), join, map(log), map(JSON.parse), getItem
-);
-
-
-applyPreferences('preferences').unsafePerformIO();
-// Object {backgroundColor: "green"}
-// <div style="background-color: 'green'"/>
-
-
-  */
-
-    // IO(console.log(x);)
+    print(
+        Maybe(Just(2))
+        .chain(identity)
+        .map(foo)
+        .toString()
+    ); // Maybe.Just(8)
 
 }());
+
+/*
+
+1. A functor is a type that implements map.
+2. An applicative is a type that implements ap.
+3. A monad is a type that implements flatMap.
+
+Array implements map, so it’s a functor.
+Promise implements map and flatMap through then, so it is a functor and a monad.
+
+*/
+
+/*
+
+1. functors: you apply a function to a wrapped value.
+2. applicatives: you apply a wrapped function to a wrapped value.
+3. monads: you apply a function that returns a wrapped value, to a wrapped value.
+
+*/
